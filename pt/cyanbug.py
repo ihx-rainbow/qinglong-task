6577d489a57d866024b94b5b3f749a885d794717
caoyu <754212163@qq.com>
2025-08-27 18:33:38 +0800

    Add check-in scripts

---
+++ pt/cyanbug.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+name: cyanbug checkin
+cron: 28 9 * * *
+"""
+import os
+
+import requests
+import re
+import random
+import time
+from util import notify
+
+def checkin():
+    if os.getenv("CYANBUG_COOKIE"):
+        cookie = os.getenv("CYANBUG_COOKIE")
+    else:
+        print("CYANBUG_COOKIE environment variable not set")
+        return
+
+    headers = {
+        "upgrade-insecure-requests": "1",
+        "dnt": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-user": "?1",
+        "sec-fetch-dest": "document",
+        "cache-control": "no-cache",
+        "cookie": cookie,
+    }
+
+    response = requests.get(
+        f"https://cyanbug.net/attendance.php",
+        headers=headers,
+    )
+    text = response.text
+    if response.status_code == 200 and "签到成功" in text:
+        # 已连续签到 <b>39</b> 天，本次签到获得 <b>3900</b> 个魔力值
+        checkin_day = re.findall(r"已连续签到 <b>(\d+)</b> 天", text)
+        magic_count = re.findall(r"本次签到获得 <b>(\d+)</b> 个魔力值", text)
+        print(f"cyanbug 签到成功！连续签到: {checkin_day} 天, 获得魔力值: {magic_count}")
+        notify.send('cyanbug 签到成功！', f'连续签到: {checkin_day} 天, 获得魔力值: {magic_count}')
+    else:
+        print(f"cyanbug 签到失败，状态码: {response.status_code}, 错误信息: {response.text}")
+        notify.send('cyanbug 签到失败！', 'cyanbug 签到失败！')
+
+
+def main():
+    # 随机延迟 1-10 秒
+    delay = random.randint(1, 10)
+    print(f"等待 {delay} 秒后开始签到...")
+    time.sleep(delay)
+    checkin()
+
+
+if __name__ == "__main__":
+    main()
